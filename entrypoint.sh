c1d5a3baaa43fd83f62467cc0f0dfc25f0c26fe1
Mark Wilson <81990+markwilson@users.noreply.github.com>
2022-01-03 16:13:20 +0000

    Include background in output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ DST="$GITHUB_WORKSPACE/dist/$2.pdf"
 
 mkdir -p $(dirname $DST)
 
-/usr/local/bin/chrome-headless-render-pdf --url="file://$SRC" --pdf="$DST" --chrome-option=--no-sandbox
+/usr/local/bin/chrome-headless-render-pdf --url="file://$SRC" --pdf="$DST" --chrome-option=--no-sandbox --include-background
